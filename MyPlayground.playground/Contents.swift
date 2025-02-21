c8979666e298d9e1cc94eb22d842f2a5844a3600
rahulp <rahulp@qburst.com>
2025-02-21 17:31:55 +0530

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// This is a comment
+
+/* Hello this
+ is
+ multi
+ line
+ */
+
+var greeting = "Hello, playground"
+
+print("Hello all")
+print("Hello \(greeting)")
+
+var a = 8
+var b = 5
+
+a = a+b
+b = a-b
+a = a-b
+
+print(a)
+print(b)
+
+var numbers = [45, 73, 195, 53]
+var computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3]]
+print(numbers[0])
+print(computedNumbers)
